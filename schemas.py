7c3f4c294589a43bfc746b4d7520496413aeebe6
Jon Hayward <jon@thehaywards.me>
2019-12-22 22:36:46 +0000

    Updated Database schema
    Updated database to match output from thetvdb.com

--- schemas.py
+++ schemas.py
@@ -4,8 +4,33 @@ from pydantic import BaseModel
 
 
 class SeriesBase(BaseModel):
-    series: str
-    summary: str = None
+    series_id: int
+    series_name: str
+    aliases: str
+    season: int
+    poster: str
+    banner: str
+    fanart: str
+    status: str
+    firstaired: str
+    network: str
+    networkid: str
+    runtime: int
+    language: str
+    genre: str
+    overview: str
+    lastupdated: str
+    airsdayofweek: str
+    airstime: str
+    rating: str
+    imdbid: str
+    zap2itid: str
+    added: str
+    addedby: str
+    siterating: str
+    siteratingcount: str
+    slug: str
+
 
 
 class SeriesCreate(SeriesBase):
